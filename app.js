1c0bb087d261619a8d6bfb0fa3e0fb6601ed5097
linuxpete711 <56452229+linuxpete711@users.noreply.github.com>
2021-01-12 12:30:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello P!'));
+app.get('/', (req, res) => res.send('Hello Pet!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
